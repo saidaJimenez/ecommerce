72aece21537b0ae31d7caa4cc4dfe8a9aff74486
Saida Jimenez <saidajimenez03@gmail.com>
2023-04-17 21:53:56 +0000

    formulario de registro terminado

--- src/utils/constanst.js
+++ src/utils/constanst.js
@@ -1,6 +1,12 @@
 export const ENV ={
     SERVER_HOST:"http://localhost:1337",
     API_URL:"http://localhost:1337/api",
-    ENDPOINTS:{},
+    ENDPOINTS:{
+        AUTH:{
+            REGISTER:"auth/local/register",
+            LOGIN:"auth/local",
+
+        },
+    },
 
 }
