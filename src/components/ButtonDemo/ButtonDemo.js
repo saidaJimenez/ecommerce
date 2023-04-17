84f8b135a19df6dec1bdf9a1ec56049779441658
Saida Jimenez <saidajimenez03@gmail.com>
2023-04-17 17:59:21 +0000

    avances

---
+++ src/components/ButtonDemo/ButtonDemo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from "./Button.module.scss";
+
+export  function ButtonDemo() {
+  return (
+    <div >
+      <p className={styles.textColor}> Hola Mundo</p>
+    </div>
+  )
+}
