dca02054c24dbb5e8196ad86331fdbbd01d66650
Saida Jimenez <saidajimenez03@gmail.com>
2023-04-17 19:59:21 +0000

    Diseño de pagina de login y sign-up

---
+++ src/pages/join/sign-up/sign-up.js
@@ -0,0 +1,15 @@
+import {JoinLayout} from "@/layouts"
+import styles from "./sign-up.module.scss";
+
+
+export default function SignUpPage() {
+  return (
+    <>
+    <JoinLayout>
+    <div>
+      <h3>Crear Cuenta </h3>
+    </div>
+    </JoinLayout>
+    </>
+  )
+}
