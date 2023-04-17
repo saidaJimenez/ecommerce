82c2fdb2c1447e4135930b6b1f7a60fcff437b3a
Saida Jimenez <saidajimenez03@gmail.com>
2023-04-17 18:15:24 +0000

    provando sass

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import 'semantic-ui-css/semantic.min.css'
-import '@/styles/globals.css'
+import "@/scss/global.scss";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
-import { ButtonDemo } from "@/components/ButtonDemo";
+
 export default function Index() {
   return (
     <div>
       <h2>
         SmellGood Shop
       </h2>
-    <ButtonDemo/>
+   
     </div>
   )
 }
