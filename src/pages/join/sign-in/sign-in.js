fa8f1132a9f7c2b83b191f32d53706ddd1df1c02
Saida Jimenez <saidajimenez03@gmail.com>
2023-04-18 08:45:28 +0000

    creando sign-iny sign-up form

---
+++ src/pages/join/sign-in/sign-in.js
@@ -0,0 +1,11 @@
+import styles from "./sign-in.module.scss";
+
+export default function SignInPage() {
+  return (
+    <>
+    <div className={styles.signIn}>
+      <h3> Iniciar sesión</h3>
+    </div>
+    </>
+  )
+}
---
+++ src/pages/join/sign-up/sign-up.js
@@ -0,0 +1,11 @@
+import styles from "./sign-up.module.scss";
+
+export default function SignUpPage() {
+  return (
+   <>
+   <div>
+    <h3> Crear cuenta </h3>
+   </div>
+   </>
+  );
+}
