2a862a03b520f702365d429efdffafe4ccf2bfd3
Saida Jimenez <saidajimenez03@gmail.com>
2023-04-18 08:26:06 +0000

    actualizado

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,6 @@
 import 'semantic-ui-css/semantic.min.css'
 import "@/scss/global.scss";
 
-export default function App(props) {
-  const { Component, pageProps} = props
-  return
-   <Component {...pageProps} />
+export default function App({ Component, pageProps}) {
+  return  <Component {...pageProps} />;
 }
