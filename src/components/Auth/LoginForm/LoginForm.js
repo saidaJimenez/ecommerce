9327e10bafc51dc06511a5a36a392c397f45bb5b
Saida Jimenez <saidajimenez03@gmail.com>
2023-04-18 10:36:13 +0000

    formulario de iniciar sesion

--- src/components/Auth/LoginForm/LoginForm.js
+++ src/components/Auth/LoginForm/LoginForm.js
@@ -1,16 +1,45 @@
 import{ Form } from "semantic-ui-react";
+import {useFormik} from "formik";
+import {initialValues, validationSchema} from "./LoginForm.form"
 
 export  function LoginForm() {
+    const formik = useFormik({
+        initialValues:initialValues(),
+        validationSchema:validationSchema(),
+        validateOnChange: false,
+        onSubmit: async (formValue)=>{
+            try {
+                console.log("FORMULARIO ENVIADO")
+                console.log(formValue);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    })
   return (
-    <Form>
-     <Form.Input name="identifier"
-      type="text" 
+    <Form onSubmit={formik.handleSubmit}>
+    <Form.Input 
+      name="identifier"
+      type="text"
       placeholder="Correo electronico o nombre de usuario"
+      value={formik.values.identifier}
+      onChange={formik.handleChange}
+      error={formik.errors.identifier}
       />
-      <Form.Input name="pasword" type="password" placeholder="Contraseña" />
-      <Form.Button type="submit" fluid>
-        Entrar
-      </Form.Button>
-    </Form>
+
+    <Form.Input
+      name="password"
+      type="password"
+      placeholder="Contraseña"
+      values={formik.values.password}
+      onChange={formik.handleChange}
+      error={formik.errors.password}
+      
+      />
+
+    <Form.Button type="submit" fluid loading={formik.isSubmitting}>
+      Entrar
+    </Form.Button>  
+  </Form>
   )
 }
