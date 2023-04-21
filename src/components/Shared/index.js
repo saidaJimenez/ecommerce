150eb3606a512d60446dfcdcd80659600323c57d
Saida Jimenez <saidajimenez03@gmail.com>
2023-04-21 22:17:35 +0000

    BarTrust

--- src/components/Shared/index.js
+++ src/components/Shared/index.js
@@ -2,4 +2,5 @@ export * from "./Separator";
 export * from "./BasicModal";
 export * from "./Confirm";
 export * from "./Label";
-export * from "./GridContenidos"
+export * from "./GridContenidos";
+export * from "./BarTrust"
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,7 +1,7 @@
 import { Container } from "semantic-ui-react"
 import {BasicLayout} from "@/layouts";
 import {Home} from "@/components/Home";
-import { Separator } from "@/components/Shared"
+import { Separator ,BarTrust } from "@/components/Shared"
 
 export default function HomePage() {
   return (
@@ -16,6 +16,11 @@ export default function HomePage() {
     </Container>
 
    
+   <Separator height={100}/>
+
+    <BarTrust/>
+   <Separator height={100}/>
+
     </BasicLayout>
     </>
   )
