015919ba05b5ec6c396d3119ca699d8e38269d0f
Saida Jimenez <saidajimenez03@gmail.com>
2023-04-22 13:47:38 +0000

    finalizado sistema de busqueda

--- src/pages/search/search.js
+++ src/pages/search/search.js
@@ -1,8 +1,12 @@
 import {useEffect} from "react";
+import { Container } from "semantic-ui-react";
+import { size } from "lodash";
 import { BasicLayout } from "@/layouts";
+import {GridContenidos, NoResult, Pagination, Separator} from "@/components/Shared"
 
 export default function SearchPage(props) {
-    console.log(props)
+    const {contenidos, pagination, searchText} = props;
+    const hasResult = size(contenidos) > 0;
 
     useEffect(() => {
     document.getElementById("search-contenido").focus()
@@ -10,7 +14,21 @@ export default function SearchPage(props) {
   return (
     <>
     <BasicLayout relative isOpenSearch>
-        <h2>Estamos en la busqueda </h2>
+        <Container>
+            <Separator height={50}/>
+
+            <h2>Buscando: {searchText}</h2>
+            {hasResult ?(
+                <>
+                <GridContenidos contenidos={contenidos}/>
+                <Separator height={30}/>
+                <Pagination currentPage={pagination.page} totalPages={pagination.pageCount}/>
+                </>
+            ): (
+                <NoResult text="No se han encontrado resultados"/>
+            )}
+            <Separator height={100}/>
+        </Container>
     </BasicLayout>
     </>
   )
