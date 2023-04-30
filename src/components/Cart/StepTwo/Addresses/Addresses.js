5b6b5395a8a2ea2b95310b4a3012df08c2979b60
Saida Jimenez <saidajimenez03@gmail.com>
2023-04-30 22:56:47 +0000

    direcciones

--- src/components/Cart/StepTwo/Addresses/Addresses.js
+++ src/components/Cart/StepTwo/Addresses/Addresses.js
@@ -1,11 +1,14 @@
 import {useState, useEffect} from "react"
+import {map} from "lodash";
+import classNames from "classnames";
 import {Address} from "@/api";
 import { useAuth } from "@/hooks"
 import styles from "./Addresses.module.scss"
 
 const addressCtrl = new Address()
 
-export  function Addresses() {
+export  function Addresses(props) {
+    const { addressSelected,  setAddressSelected} = props
     const [addresses, setAddresses] = useState(null);
     const {user} = useAuth()
     console.log(addresses)
@@ -14,15 +17,27 @@ export  function Addresses() {
     (async () => {
         try {
             const response = await addressCtrl.getAll(user.id);
-            setAddresses(response)
+            setAddresses(response.data)
         } catch (error) {
             console.error(error)
         }
     })()
     }, [])
   return (
-    <div>
-      <h2>Addresses</h2>
+    <div className={styles.addresses}>
+      <h2>Dirección</h2>
+      {map(addresses, (address) => (
+        <div key={address.id} className={classNames(styles.address, {
+            [styles.active]: address.id === addressSelected?.id,
+        })} 
+        onClick={() => setAddressSelected(address)}>
+            <p>{address.attributes.name}({address.attributes.title})</p>
+            <p>
+                {address.attributes.address}, {address.attributes.postal_code},
+                {address.attributes.state}, {address.attributes.city}
+            </p>
+        </div>
+      ))}
     </div>
   )
 }
--- src/components/Cart/StepTwo/StepTwo.js
+++ src/components/Cart/StepTwo/StepTwo.js
@@ -1,15 +1,19 @@
+import {useState} from "react"
 import {Separator} from "@/components/Shared";
 import {Addresses} from "./Addresses"
 import styles from "./StepTwo.module.scss"
 
 export  function StepTwo(props) {
-    const {contenidos} = props
+    const {contenidos} = props;
+    const [addressSelected, setAddressSelected] = useState(null)
+    
   return (
     <div className={styles.stepTwo}>
       <div className={styles.center}>
-    <p>ADDRESSES</p>
+      <Addresses  addressSelected={addressSelected} setAddressSelected={setAddressSelected}/>
     <Separator heigth={50}/>
-    <Addresses/>
+    {addressSelected &&  <p>PAYMEN</p> }
+    
       </div>
       <div className={styles.rigth}>
         <p>Resume</p>
